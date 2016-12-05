e9af9883e3c9346580f17fcc72e140710b96b061
GibleyBoo <te15.jonas.obrell@umea.nti.se>
2016-12-05 12:51:28 +0100

    memes
    Renamed all pictures, removed scrollbar, additional

--- konst_script.js
+++ konst_script.js
@@ -1,8 +1,19 @@
 $(document).ready(function(){
 
+
   $(".text-toggle").click(function(){
+    //  Change button up/down every toggle
+    var menuChange = $(this).children().hasClass("glyphicon-menu-up");
+    if (!menuChange) {
+      $(this).children().removeClass("glyphicon-menu-down").addClass("glyphicon-menu-up");
+    } else {
+      $(this).children().removeClass("glyphicon-menu-up").addClass("glyphicon-menu-down");
+    }
+    //  Toggle text content
     $(this).siblings(".textInfo").toggle(500);
+
   });
 
+  
   //functions ...
 });
